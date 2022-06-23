22b3c75c749db5543af6c1620ffa7159fc45f2fe
manhha591 <manhha2k1@gmail.com>
2022-06-23 19:42:16 +0700

    qlvb

---
+++ src/btqlvb/main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package btqlvb.main;
+import btqlvb.view.MainJFrame;
+        
+        
+
+/**
+ *
+ * @author Manh Ha
+ */
+public class Main {
+    public static void main(String[] args) {
+        new MainJFrame().setVisible(true);
+    }
+}
